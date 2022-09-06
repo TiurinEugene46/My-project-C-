bfb6adc0520dce61f926647acc5a661d462c98f8
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-06 13:51:40 +0300

    Example My Project

---
+++ MyProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// Определение входного числа
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Получение квадрата входного числа
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+// Получение квадрата числа матемотической функцией
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
