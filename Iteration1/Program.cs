f632b76bedc21633ea19b4441853fd6485323c88
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-06 15:06:22 +0300

    Iteration 1

---
+++ Iteration1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Определение входных чисел
+Console.WriteLine("Введиnе числа: ");
+int result = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
+
+// Условие получения квадрата второго входного числа
+if(result == number * number)
+{
+    Console.WriteLine($"{result} квадрат числа {number}");
+}
+else
+{
+    Console.WriteLine($"{result} не является квадратом числа {number}");
+}
